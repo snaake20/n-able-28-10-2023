bd07fd23e5b1ae83bc05f2022bf61b248ef9454b
Mihai Rosu <rosumihai16@gmail..com>
2023-10-28 09:14:54 +0300

    initial firebase-config

---
+++ pocket-doctor/src/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJIzu81vxkNqtbLhHBHDSnjKIySV8WZY8",
+  authDomain: "pocket-doctor-3bd2d.firebaseapp.com",
+  projectId: "pocket-doctor-3bd2d",
+  storageBucket: "pocket-doctor-3bd2d.appspot.com",
+  messagingSenderId: "692417736583",
+  appId: "1:692417736583:web:e6005086594197f99d6d22",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const database = getFirestore(app);
+export const auth = getAuth(app);
