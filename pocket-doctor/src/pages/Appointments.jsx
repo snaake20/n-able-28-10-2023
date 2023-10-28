ac4dafd6fdba0e83b9bb894667582f3f200be4b2
Mihai Rosu <rosumihai16@gmail..com>
2023-10-28 18:38:44 +0300

    styling appointments

--- pocket-doctor/src/pages/Appointments.jsx
+++ pocket-doctor/src/pages/Appointments.jsx
@@ -2,7 +2,7 @@ import AppointmentCalendar from "../components/calendar/AppointmentCalendar";
 import AppointmentsList from "../components/calendar/AppointmentsList";
 
 export default function Appointments() {
-  return <div className="py-[10vh]">
+  return <div className="py-[10vh] flex justify-center flex-col items-center">
     <AppointmentCalendar />
 
     <AppointmentsList />
